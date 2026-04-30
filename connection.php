65291d4e3ce5e0df82b67e939f7cf7b5431e7aae
Krista <simbilys2.0@gmail.com>
2026-04-30 15:41:29 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,12 +1,12 @@
 <?php
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'student_db';
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "your_database_name";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
